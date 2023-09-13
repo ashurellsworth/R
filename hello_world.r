c908ec58aadcd8c14b1c15a886234ae6e7980aba
ashurellsworth <ashurellsworth100@gmail.com>
2023-09-13 14:20:55 -0600

    New R file

---
+++ hello_world.r
@@ -0,0 +1,10 @@
+#Using the print() function
+print("Hello World!")
+
+#Do this to supress the quotes in the output
+print("Hello World!", quote = FALSE)
+
+#Can split and paste the words individually using paste()
+print(paste("Hello", "World", "!"))
+
+
